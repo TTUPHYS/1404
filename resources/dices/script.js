2c3ee0251ecd232f4ce051cc0d106020a75e152f
TTUPHYS <ttuphys@gmail.com>
2024-10-31 20:59:02 -0500

    Update script.js

--- resources/dices/script.js
+++ resources/dices/script.js
@@ -1,49 +1,72 @@
 const buttonEl = document.getElementById("roll-button");
+const buttonEl2 = document.getElementById("num-button");
+
 const diceEl = document.getElementById("dice");
 const rollHistoryEl = document.getElementById("roll-history");
 
 let historyList = [];
+let maxNum=document.getElementById("num").value;
+let iniNumDices=document.getElementById("numDices").value;
+
 
 function rollDice() {
-  const rollResult = Math.floor(Math.random() * 10) + 1;
-  const diceFace = rollResult
-  diceEl.innerHTML = diceFace;
-  historyList.push(rollResult);
+  let tot=Math.floor(iniNumDices);
+  let rollResult;
+  for (let i = 0; i<iniNumDices;i++){
+    rollResult = Math.floor(Math.random() * maxNum) + 1;
+    if (rollResult==1){
+        tot--;
+    }
+    // historyList.push("tot"+tot);
+    // historyList.push("rollResult"+rollResult);
+  }
+  historyList.push(tot);
+  //const rollResult = Math.floor(Math.random() * maxNum) + 1;
+  //const diceFace = getDiceFace(rollResult);
+  //diceEl.innerHTML = diceFace;
+  //historyList.push(tot);
+  iniNumDices=tot;
   updateRollHistory();
 }
 
 function updateRollHistory() {
   rollHistoryEl.innerHTML = "";
-  for (let i = 0; i < historyList.length; i++) {
+  for (let i = historyList.length-1; i >=0; i--) {
     const listItem = document.createElement("li");
-    listItem.innerHTML = `Roll ${i + 1}: <span>${historyList[i]}</span>`;
+    listItem.innerHTML = `Roll ${i + 1}: <span>${getDiceFace(historyList[i])}</span>`;
     rollHistoryEl.appendChild(listItem);
   }
 }
 
-// function getDiceFace(rollResult) {
-//   switch (rollResult) {
-//     case 1:
-//       return "&#9856;";
-//     case 2:
-//       return "&#9857;";
-//     case 3:
-//       return "&#9858;";
-//     case 4:
-//       return "&#9859;";
-//     case 5:
-//       return "&#9860;";
-//     case 6:
-//       return "&#9861;";
-//     default:
-//       return "";
-//   }
-// }
+function getDiceFace(rollResult) {
+    return rollResult;
+  switch (rollResult) {
+    case 1:
+      return "&#9856;";
+    case 2:
+      return "&#9857;";
+    case 3:
+      return "&#9858;";
+    case 4:
+      return "&#9859;";
+    case 5:
+      return "&#9860;";
+    case 6:
+      return "&#9861;";
+    default:
+      return "";
+  }
+}
+
+buttonEl2.addEventListener("click", () => {
+  maxNum=document.getElementById("num").value;
+  iniNumDices=document.getElementById("numDices").value;
+  historyList = [];
+  diceEl.innerHTML = "&#9861;";
+  //historyList.push(maxNum);
+  updateRollHistory();
+});
 
 buttonEl.addEventListener("click", () => {
-  diceEl.classList.add("roll-animation");
-  setTimeout(() => {
-    diceEl.classList.remove("roll-animation");
-    rollDice();
-  }, 1000);
+  rollDice();
 });
