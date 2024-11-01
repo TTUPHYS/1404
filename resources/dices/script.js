33c4c40750c7836c0ca89267a97e10e884a2df7a
TTUPHYS <ttuphys@gmail.com>
2024-10-31 19:38:07 -0500

    Update script.js

--- resources/dices/script.js
+++ resources/dices/script.js
@@ -21,24 +21,24 @@ function updateRollHistory() {
   }
 }
 
-function getDiceFace(rollResult) {
-  switch (rollResult) {
-    case 1:
-      return "&#9856;";
-    case 2:
-      return "&#9857;";
-    case 3:
-      return "&#9858;";
-    case 4:
-      return "&#9859;";
-    case 5:
-      return "&#9860;";
-    case 6:
-      return "&#9861;";
-    default:
-      return "";
-  }
-}
+// function getDiceFace(rollResult) {
+//   switch (rollResult) {
+//     case 1:
+//       return "&#9856;";
+//     case 2:
+//       return "&#9857;";
+//     case 3:
+//       return "&#9858;";
+//     case 4:
+//       return "&#9859;";
+//     case 5:
+//       return "&#9860;";
+//     case 6:
+//       return "&#9861;";
+//     default:
+//       return "";
+//   }
+// }
 
 buttonEl.addEventListener("click", () => {
   diceEl.classList.add("roll-animation");
